3d4030b324bc2cb9bccfa4aec9fa26e78188d665
Anton Holmes <holmesanton@icloud.com>
2018-04-24 13:50:51 -0400

    finished cat without comments

--- cat.js
+++ cat.js
@@ -1,24 +1,10 @@
 
 const fs = require('fs')
 let pwd = require('./pwd.js')
-// let args = process.argv.splice(2,process.argv.length-1)
 module.exports = function(file){
-  // console.log('this is: ' + process.stdout.write(process.cwd()))
-  console.log('this is too: ' + process.cwd() + '/' + file)
-  // console.log('this one is: ' + pwd() + '/' + file)
   fs.readFile((process.cwd())+ '/' + file, 'utf8', (err, data) => {
-    console.log({data});
     if (err) throw 'whatever';
     console.log(data);
-    // process.stdout.write(files.join('\n'))
-    // process.stdout.write('\nprompt > ');
-    // args.forEach(line => function(line){
-  //   fs.readFile('file')
+    process.stdout.write('prompt > ');
     });
   };
-  // fs.readdir('./', 'utf8', (err, files) => {
-  //   if (err) {
-  //     throw err
-  //   } else {
-
-
